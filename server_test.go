641eefdb528c5c26649cb4c7db1dc1de16a1308f
Christian Höltje <docwhat@gerf.org>
2016-09-19 20:18:19 -0400

    Added test for urlExists()

---
+++ server_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func testURLExists(t *testing.T) {
+	var urlTestTable = []struct {
+		in  string
+		out bool
+	}{
+		{"https://google.com/", true},
+		{"https://doesnotexist.example.com/", false},
+	}
+
+	for _, tt := range urlTestTable {
+		exists := urlExists(tt.in)
+		if exists != tt.out {
+			t.Errorf("urlExists(%v) => %v, want %v", tt.in, exists, tt.out)
+		}
+	}
+}
