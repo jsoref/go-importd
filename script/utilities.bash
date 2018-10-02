faf437911b39d1fc701ab8f528f82f283d24a108
Christian Höltje <docwhat@gerf.org>
2018-10-01 23:26:52 -0400

    travis: always use 1.11 modules

--- script/utilities.bash
+++ script/utilities.bash
@@ -2,26 +2,23 @@
 #
 # Utility functions for pretty output
 
-cd "$(dirname "$0")"
-cd ..
+cd "$(dirname "${BASH_SOURCE[0]}")" && cd ..
 
 sent_header=f
-colors=$(( $(tput colors 2> /dev/null || :) + 0 ))
+colors=$(($(tput colors 2>/dev/null || :) + 0))
 
-function colorize()
-{
-  if (( colors >= 8 )); then
+function colorize() {
+  if ((colors >= 8)); then
     tput bold
     tput setaf 4
   fi
   cat -
-  if (( colors >= 8 )); then
+  if ((colors >= 8)); then
     tput sgr0
   fi
 }
 
-function header()
-{
+function header() {
   if [ "$sent_header" = t ]; then
     echo
   fi
@@ -30,9 +27,13 @@ function header()
   sent_header=t
 }
 
-function join_by()
-{
-  local IFS="$1"; shift; echo "$*";
+function join_by() {
+  local IFS="$1"
+  shift
+  echo "$*"
 }
+
+# We want go 1.11 to always use module support.
+export GO111MODULE=on
 
 # vim: set ft=sh :
